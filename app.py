4036749b12ba6d4a97ffe53177460f2572c00dd4
GabbrM <115942603+GabbrM@users.noreply.github.com>
2023-06-27 14:45:21 -0500

    index added

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def integrantes():
 
 @app.route('/index')
 def index():
-    return render_template('inicio.html')
+    return render_template('index.html')
 
 @app.route('/membresias')
 def membresias():
